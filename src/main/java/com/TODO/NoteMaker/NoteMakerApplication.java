57a67b12147db6f78e5767e1dfb63ee7ab49c66e
lohith-jalla <lohithjalla12@gmail.com>
2024-11-13 14:34:49 +0530

    Made some changes in the Controllers

--- src/main/java/com/TODO/NoteMaker/NoteMakerApplication.java
+++ src/main/java/com/TODO/NoteMaker/NoteMakerApplication.java
@@ -2,7 +2,11 @@ package com.TODO.NoteMaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.TODO.NoteMaker")
+@EntityScan(basePackages = "com.TODO.NoteMaker")
 @SpringBootApplication
 public class NoteMakerApplication {
 
--- src/main/java/com/TODO/NoteMaker/noteController.java
+++ src/main/java/com/TODO/NoteMaker/noteController.java
@@ -2,35 +2,83 @@ package com.TODO.NoteMaker;
 
 import java.util.*;
 import com.TODO.NoteMaker.model.note;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class noteController {
 
-    noteRepository repo;
+    private final Services s;
 
-    public noteController(noteRepository repo) {
-        this.repo = repo;
+    public noteController(Services s) {
+        this.s = s;
     }
 
-    @GetMapping("/findById")
-    public Optional<note> Home(@RequestParam Long id) {
-        return repo.findById(id);
+    //HomePage ***********************************
+    @GetMapping("/index")
+    public String home(){
+        return "index";
     }
 
-    @PostMapping("/save")
-    public void save(@RequestBody note n){
-        repo.save(n);
+    // Create Note Mapping ***********************
+    @GetMapping("/create")
+    public String createNote() {
+        return "save";
     }
 
-    @GetMapping("/All")
-    public List<note> findAll(){
-        return repo.findAll();
+    @PostMapping("/create")
+    public String PostCreate(@RequestBody note note) {
+        s.save(note);
+        return "redirect:/index";
     }
 
-    @DeleteMapping("/deleteById")
-    public void deleteById(@RequestParam Long id){
-        repo.deleteById(id);
+    //Display Page *********************************
+    @GetMapping("/AllNotes")
+    public String findAll(Model model){
+        List<note> notes = s.getAllNotes();
+        model.addAttribute("notes", notes);
+        return "AllNotes";
     }
 
+    //Update Page************************************
+    @GetMapping("/update")
+    public String update(ModelMap map, @RequestParam int id){
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String Patchupdate(ModelMap map, @RequestParam Long id , @RequestParam String title, @RequestParam String content){
+        boolean noteExist=s.exist(id);
+        if(!noteExist) {
+            map.put("error", "Note Not Exist");
+            return "index";
+        }else{
+            note n=new note();
+            n.setTitle(title);
+            n.setContent(content);
+            s.save(n);
+            return "redirect:/AllNotes";
+        }
+    }
+
+    //DeletePage ***********************************
+    @GetMapping("/deleteById")
+    public String deleteById(@RequestParam int id){
+        return "delete";
+    }
+
+    @PostMapping("/deleteById")
+    public String deleteById(ModelMap map,@RequestParam Long id){
+        boolean noteExist=s.exist(id);
+        if(!noteExist) {
+            map.put("error`", "Note Not Exist");
+            return "index";
+        }
+        else {
+            s.delete(id);
+            return "redirect:/AllNotes";
+        }
+    }
 }
